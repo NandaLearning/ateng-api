a75e12f0db9e67eafcf0b36f50d9a22f8a9db9c6
NANDA <NANDA@gmail.com>
2023-12-13 00:22:04 -0800

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 app.use(cors())
 
 const data = [
-  {name: "Ateng Api cuyy 😎", anggota: 7 ,image:"https://scontent.fcgk43-1.fna.fbcdn.net/v/t1.18169-9/20046300_117010978923363_7675206835184950992_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=2be8e3&_nc_eui2=AeHKFbREpwQ8a8t60Qmt4Lq3a_IBH59pDlZr8gEfn2kOVkwAHfW9GQIiMOpCFYNYoq5SmX4ULg3JSV-_TLLxC1fJ&_nc_ohc=v4NUNrFMRjUAX8ABKMr&_nc_oc=AQkfgH-k65sugC7MJBt8lmClbHAnSa_mqzxmaH1TN6Xrp1g-DCVZAWq7yOrGgwnxWkA&_nc_ht=scontent.fcgk43-1.fna&oh=00_AfBFdQ6EjnB1zRjNg-kk9sEZdg2Qx_fcJbnFceYvTvbGgQ&oe=65A0A37B"},
+  { name: "Ateng Api cuyy 😎", anggota: 7 ,image:"https://media-xsp1-2.cdn.whatsapp.net/v/t61.24694-24/312240005_940587030689958_2275050323242277943_n.jpg?ccb=11-4&oh=01_AdSDrXuxSDxM7Qb_MIT_Tx4Z17DVa9qvhBE9MKmmmd_JIA&oe=65844165&_nc_sid=e6ed6c&_nc_cat=108"},
 ]
 
 // routes utama method (get) / endpoint
