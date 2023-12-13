e98a3c5f6d68a4e01f4fca32d686d18f70df4e20
NANDA <NANDA@gmail.com>
2023-12-13 00:25:05 -0800

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 app.use(cors())
 
 const data = [
-  { name: "Ateng Api cuyy 😎", anggota: 7 ,image:"https://media-xsp1-2.cdn.whatsapp.net/v/t61.24694-24/312240005_940587030689958_2275050323242277943_n.jpg?ccb=11-4&oh=01_AdSDrXuxSDxM7Qb_MIT_Tx4Z17DVa9qvhBE9MKmmmd_JIA&oe=65844165&_nc_sid=e6ed6c&_nc_cat=108"},
+  { name: "Ateng Api cuyy 😎", anggota: 7 ,image:"./ateng.jpeg"},
 ]
 
 // routes utama method (get) / endpoint
