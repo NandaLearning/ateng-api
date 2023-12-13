c663565720613058dd5cb786a5caba3eec2a4055
NANDA <NANDA@gmail.com>
2023-12-13 08:24:54 -0800

    first commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors())
 
 const data = [
   { name: "Ateng Api cuyy 😎", anggota: 7 ,image:"./ateng.jpeg"},
+  { slide: "https://images.tokopedia.net/img/cache/1190/wmEwCC/2023/12/12/f5347ee5-0ce3-4b76-bf24-0ef05e6c22c3.jpg.webp?ect=4g" , slide: "https://images.tokopedia.net/img/cache/1190/wmEwCC/2023/12/12/f5347ee5-0ce3-4b76-bf24-0ef05e6c22c3.jpg.webp?ect=4g" , slide : "https://images.tokopedia.net/img/cache/1200/NXCtjv/2023/12/9/ef5c2f59-3a39-4120-9b38-c613c0e27baa.jpg"}
 ]
 
 // routes utama method (get) / endpoint
